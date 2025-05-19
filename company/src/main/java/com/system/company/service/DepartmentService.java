d4e34beacc47fc0e5c3dcaa6f37b917ac0194327
Udara Nalawansa <udarakavishka13@gmail.com>
2025-05-20 02:12:32 +0530

    refactor: Remove unused import for RequestBody in DepartmentService

--- company/src/main/java/com/system/company/service/DepartmentService.java
+++ company/src/main/java/com/system/company/service/DepartmentService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.system.company.model.Department;
 import com.system.company.repo.DepartmentRepo;
